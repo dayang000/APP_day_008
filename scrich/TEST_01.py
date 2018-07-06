804cbd6e58e93f1b5ccb4b489d3e9d71b2dff607
dayang000 <1546822962@qq.com>
2018-07-06 17:11:52 +0800

    项目02

--- scrich/TEST_01.py
+++ scrich/TEST_01.py
@@ -1,3 +1,13 @@
+import allure,pytest
 class Test_001:
+    @allure.step(title="测试步骤001")
     def test_01(self):
+        allure.attach("描述","输入用户名")
+        assert 1
+    # @pytest.allure.severity(pytest.allure.severity_level.BLOCKED)
+
+    @allure.step(title="测试步骤002")
+    def test_02(self):
+        allure.attach("描述", "输入密码")
         assert 0
+
