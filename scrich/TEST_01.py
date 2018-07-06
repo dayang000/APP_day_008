67eaf86d91ab2af4e9e3e0caeb795d605a882d12
dayang000 <1546822962@qq.com>
2018-07-06 19:42:49 +0800

    添加了等级

--- scrich/TEST_01.py
+++ scrich/TEST_01.py
@@ -4,8 +4,8 @@ class Test_001:
     def test_01(self):
         allure.attach("描述","输入用户名")
         assert 1
-    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
 
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @allure.step(title="测试步骤002")
     def test_02(self):
         allure.attach("描述", "输入密码")
